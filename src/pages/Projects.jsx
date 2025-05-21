930e5cfc0c5aabde864578cbe4ff2d902f1e886e
silpa4251 <silpadas4251@gmail.com>
2025-05-21 15:35:01 +0530

    disabled the unwanted lines

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -218,7 +218,7 @@ const Projects = () => {
                 />
                 <button
                   onClick={closeProjectModal}
-                  className="absolute top-4 right-4 bg-black bg-opacity-50 text-white rounded-full w-10 h-10 flex items-center justify-center text-xl hover:bg-opacity-70"
+                  className="absolute top-5 right-3 bg-black bg-opacity-50 text-white rounded-full w-10 h-10 flex justify-center text-3xl hover:bg-opacity-70"
                 >
                   ×
                 </button>
